527812935d9cb88694078b1f2f5c03b294696fa1
Matteo Bertozzo <matteobertozzo@outlook.com>
2018-10-17 15:32:11 +0200

    Live checking conditions

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,13 +4,55 @@ import styles from './style.scss';
 export class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      error: this.props.getErrorMessage('')
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
-
+  
+  handleSubmit (event) {
+    event.preventDefault();
+    const value = event.target.elements.username.value;
+    const error = this.props.getErrorMessage(value);
+    
+    if (error) {
+      alert(`error: ${error}`)
+    } else {
+      alert(`success: ${value}`)
+    }
+  }
+  
+  handleChange (event) {
+    const {value} = event.target
+    this.setState({
+      error: this.props.getErrorMessage(value),
+    })
+  }
+  
   render() {
+    const {error} = this.state
     return (
-      <section className="container">
-        <h1 className={styles.title}>Hello World!</h1>
-      </section>
-    );
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            onChange={this.handleChange}
+            name="username"
+          />
+        </label>
+        {error ? (
+          <div style={{color: 'red'}}>
+            {error}
+          </div>
+        ) : null}
+        <button
+          disabled={Boolean(error)}
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ import { App } from './components/app';
 import './globals.css';
 
 ReactDOM.render(
-  <App />,
+  <App
+    getErrorMessage={value => {
+      if (value.length < 3) {
+        return `Value must be at least 3 characters, but is only ${value.length}`
+      }
+      if (!value.includes('s')) {
+        return `Value does not include "s" but it should!`
+      }
+      return null
+    }} 
+  />,
   document.getElementById('app')
 );
+
+
